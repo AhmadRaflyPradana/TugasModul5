5659fc82d39feb0f9df2ffe50b93860e6e78ef8f
AhmadRaflyPradana <arafly560@gmail.com>
2020-04-29 10:56:17 +0700

    first commit

---
+++ Modul5.cs
@@ -0,0 +1,19 @@
+package tugas_modul5;
+
+import java.util.Scanner;
+
+public class Tugas_Modul5 {
+    public static void main(String[] args) {
+        String email, password;
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Email : ");
+        email = input.nextLine();
+        System.out.print("Password : ");
+        password = input.nextLine();
+        
+        userService satu = new userService( email, password );
+        satu.login();
+    }
+    
+}
